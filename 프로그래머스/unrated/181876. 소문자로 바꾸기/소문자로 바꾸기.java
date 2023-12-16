9ad01efc6ed24e3d738ba00e95742badf94305f2
pengsuming <128009423+pengsuming@users.noreply.github.com>
2023-12-16 19:05:32 +0900

    [level unrated] Title: 소문자로 바꾸기, Time: 12.06 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181876. 소문자로 바꾸기/소문자로 바꾸기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        int temp;
+        for(int i=0; i<myString.length(); i++){
+            temp = myString.charAt(i);
+            if(temp>=65 && temp<=90){
+                answer += (char)(temp+32);
+            }else{
+                answer += (char)temp;
+            }
+        }
+        return answer;
+    }
+}
